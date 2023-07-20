9ae543fa3303723a081a7fa91213d8a1db031308
riccardo-giuri <richicryst@gmail.com>
2023-07-20 16:52:20 +0200

    Js Docs and comments added to the code for more readablity

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 Vue.createApp({
     data() {
         return {
+            /**
+             * @type {object[]}
+             */
             slides: [
                 {
                     image: 'img/01.webp',
@@ -29,45 +32,82 @@ Vue.createApp({
                 }
             ],
 
+            /**
+             * @type {string}
+             */
             slideClass: "thumb",
+
+            /**
+             * @type {number}
+             */
             activeSlideIndex: 0,
-            autoplay: null
+
+            /**
+             * @type {number}
+             */
+            autoplay: 0
         }
     },
 
     methods: {
+        /**
+         * When the previous slide button is clicked decrement by 1 the slide index
+         */
         previousSlideClick(){
+            //check if the previous element index is less than than zero
             if(this.activeSlideIndex <= 0) {
+                //restarted the index to the last one possible
                 this.activeSlideIndex = (this.slides.length - 1)
             }
             else {
+                //else decrement it by one
                 this.activeSlideIndex--;
             }
         },
 
+        /**
+         * When the previous slide button is clicked increment by 1 the slide index
+         */
         nextSlideClick() {
+            //check if the next element index is over the number of possible index
             if(this.activeSlideIndex >= (this.slides.length - 1)) {
+                //restarted the index to the first one
                 this.activeSlideIndex = 0
             }
             else {
+                //else increment it by one
                 this.activeSlideIndex++;
             }
         },
 
+        /**
+         * Function called when the slide is clicked and activate it in the slider
+         * @param {number} index the index of the element clicked
+         */
         slideClick(index) {
+            //make the index of the active slide the same as the one clicked
             this.activeSlideIndex = index;
         },
 
+        /**
+         * Start the autoplay of the slider
+         */
         startAutoplay() {
+            //activate the timer and save it in a variable
             this.autoplay = setInterval(this.nextSlideClick, 3000);
         },
 
+        /**
+         * Block the autoplay of the slider
+         */
         stopAutoplay() {
+            //clear the timer previously started
             clearInterval(this.autoplay);
         }
     },
 
     mounted() {
+        //start the autoplay of the slider
         this.startAutoplay();
     } 
 }
